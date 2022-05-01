21c68aeb6c07642f93f8bb67470f436eb0865d49
kleyson Castro <kleysoncastro99@gmail.com>
2022-05-01 00:44:05 -0300

    add função para led indicador

--- src/main.c
+++ src/main.c
@@ -8,6 +8,18 @@
 #define MY_PRIORITY 5
 
 
+static const struct led_name_alias led0 = {
+	.spec = GPIO_DT_SPEC_GET_OR(LED0, gpios, {0}),
+};
+
+static const struct led_name_alias led1 = {
+	.spec = GPIO_DT_SPEC_GET_OR(LED1, gpios, {0}),
+};
+
+static const struct led_name_alias led2 = {
+	.spec = GPIO_DT_SPEC_GET_OR(LED2, gpios, {0}),
+};
+
 void main(void) {
 
 
@@ -15,15 +27,15 @@ void main(void) {
 } // end main
 
 void blink1(void) {
-	led_thread(500);
+	led_thread(&led0, 500);
 }
 
-/* void blink2(void) {
-	led_thread0(250);
+ void blink2(void) {
+	led_thread(&led1, 250);
 }
-void blink3(void) {
-	led_thread0(30);
-} */
+ void blink3(void) {
+	led_thread(&led2, 30);
+} 
 
 
 K_THREAD_DEFINE(name_process_led1, MY_STACK_SIZE,
@@ -31,10 +43,10 @@ K_THREAD_DEFINE(name_process_led1, MY_STACK_SIZE,
                 MY_PRIORITY, 0, 0);
 
 
-/* K_THREAD_DEFINE(name_process_led2, MY_STACK_SIZE,
+ K_THREAD_DEFINE(name_process_led2, MY_STACK_SIZE,
 blink2, NULL, NULL, NULL,
 MY_PRIORITY, 0, 0);
 
 K_THREAD_DEFINE(name_process_led3, MY_STACK_SIZE,
 blink3, NULL, NULL, NULL,
-MY_PRIORITY, 0, 0);  */
+MY_PRIORITY, 0, 0); 
