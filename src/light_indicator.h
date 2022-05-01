5a5dd93b976bebd3f27cecf5bd2569e7aef8aeea
kleyson Castro <kleysoncastro99@gmail.com>
2022-05-01 00:21:03 -0300

    fac: add novo parametro na função de acionamento de led

--- src/light_indicator.h
+++ src/light_indicator.h
@@ -12,7 +12,13 @@
 #define DELAY_MS_LED1 100
 #define DELAY_MS_LED2 20
 
-	 void led_thread(int32_t ms);
+	struct led_name_alias {
+		struct gpio_dt_spec spec;
+		const char *pgio_pin_name;
+	};
+
+	void led_thread(const struct led_name_alias *led, int32_t ms);
+
 
 
 #endif
